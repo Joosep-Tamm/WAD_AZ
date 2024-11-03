6b14c4fbeac5408edac2e5f1e3103a4901786371
JoosepTamm <joosep.tamm.001@gmail.com>
2024-11-03 15:06:18 +0200

    improved dropwdown positioning, especially with scaling

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,14 @@
 function dropdownMenu() {
-    const element = document.getElementById("dropdownMenu"); 
-    if (element.style.display === "none" || element.style.display === "") {
-        element.style.display = "block";  
+    const dropdown = document.getElementById("dropdownMenu"); 
+    const profilePic = document.querySelector(".profile-pic");
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        const rect = profilePic.getBoundingClientRect();
+        
+        dropdown.style.top = `${rect.bottom}px`;
+        dropdown.style.right = `${window.innerWidth - rect.right - profilePic.scrollWidth/2}px`;
+        
+        dropdown.style.display = "block";  
     } else {
-        element.style.display = "none";   
+        dropdown.style.display = "none";   
     }
 }
